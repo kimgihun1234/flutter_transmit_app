b7ad11a48f1b5711580b3629af083c994f0138bc
kimgihun1234 <y2k16241@naver.com>
2023-12-09 21:49:44 +0900

    exama

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button the button asdasdff',
+              'You have pushed the button the button asdasdfasdfsf',
             ),
             Text(
               '$_counter',
